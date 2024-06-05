1d2e6d702a6ca30021ec7538525b4f691e00b4e2
Huy Hiep <huyhiep4520@gmail.com>
2024-06-06 01:21:34 +0700

    test refresh token

--- server.js
+++ server.js
@@ -1,20 +1,38 @@
-const express = require('express')
-const app = express()
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const express = require("express");
+const app = express();
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
-const port = 5000
+const port = 5000;
 
-app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.get('/user', (req, res) => {
+app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get("/auth", (req, res) => {
+  const { token } = req.query;
+  if (token && +token < Date.now()) {
+    return res.json({
+      status: 401,
+    });
+  }
+  return res.json({
+    status: 200,
+    data: Date.now() + 1000 * 10,
+  });
+});
+
+app.get("/auth-user", (req, res) => {
+  return res.json({
+    status: 200,
+    data: "user",
+  });
+});
+
+app.get("/refresh", (req, res) => {
   res.json({
     status: 200,
-    description: "A list of users kk"
-  })
-})
-
-
+    description: Date.now() + 2000 * 10,
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
